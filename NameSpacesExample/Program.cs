133af572e100dcc661e35e158f34ae08901d1228
eddiePower <eddie.power@icloud.com>
2017-04-27 23:54:00 +1000

    A quick NameSpace example for C#

--- NameSpacesExample/Program.cs
+++ NameSpacesExample/Program.cs
@@ -10,6 +10,92 @@ namespace NameSpacesExample
     {
         static void Main(string[] args)
         {
+            var produce = new List<object>();
+
+            var f1 = new Fruit("Apple", 8.5, 3);
+            produce.Add(f1);
+
+            var f2 = new Fruit("Bannana", 4.5, 7);
+            produce.Add(f2);
+
+            produce.Add(new Vegetable("Carrot", 4.1, 16));
+
+            Console.WriteLine("There are " + produce.Count + " items in produce");
+
+            foreach (var item in produce)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class Fruit
+    {
+        public Fruit()
+        {
+        }
+
+        public Fruit(string name, double weight, int quantity)
+        {
+            Name = name;
+            Weight = weight;
+            Quantity = quantity;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name.ToUpper(); }
+            set { name = value; }
+        }
+
+        private double weight;
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " (" + Weight + "oz) : " + Quantity;
+        }
+    }
+
+    class Vegetable
+    {
+        public Vegetable()
+        {
+        }
+
+        public Vegetable(string name, double weight, int quantity)
+        {
+            Name = name;
+            Weight = weight;
+            Quantity = quantity;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name.ToUpper(); }
+            set { name = value; }
+        }
+
+        private double weight;
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " (" + Weight + "oz) : " + Quantity;
         }
     }
 }
