f86259161cec05475021f5f17f444e1e8eb1b388
eddiePower <eddie.power@icloud.com>
2017-04-28 22:56:33 +1000

    Final change for namespace and inherentance examples, the code duplication has now been removed and the vegtable and Fruit class both inherate from the super class Produce.

--- NameSpacesExample/Model/Produce.cs
+++ NameSpacesExample/Model/Produce.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace NameSpacesExample.Model
 {
     class Produce
@@ -67,38 +62,17 @@ namespace NameSpacesExample.Model
         }
     }
 
-    class Vegetable
+    class Vegetable : Produce
     {
         public Vegetable()
         {
+
         }
 
-        public Vegetable(string name, double weight, int quantity)
+        public Vegetable(string name, double weight, int quantity) :
+            base(name, weight, quantity)
         {
-            Name = name;
-            Weight = weight;
-            Quantity = quantity;
-        }
 
-        private string name;
-        public string Name
-        {
-            get { return name.ToUpper(); }
-            set { name = value; }
-        }
-
-        private double weight;
-        public double Weight
-        {
-            get { return weight; }
-            set { weight = value; }
-        }
-
-        public int Quantity { get; set; }
-
-        public override string ToString()
-        {
-            return Name + " (" + Weight + "oz) : " + Quantity;
         }
     }
 }
